5456084f120e59ab6c68a0afb607e1de924b0f32
korodiy <korodiy@yandex-team.ru>
2026-03-11 23:13:53 +0300

    RCA-04: Организовать структуру приложения через Scaffold

--- app/src/main/java/com/yourcompany/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.yourcompany.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +16,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeComposeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold { paddingValues ->
+                    Text("Recipes App", modifier = Modifier.padding(paddingValues))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipeComposeAppTheme {
-        Greeting("Android")
     }
 }

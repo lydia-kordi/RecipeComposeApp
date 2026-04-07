1971adc011468d419845f001b3db79bb372050ee
korodiy <korodiy@yandex-team.ru>
2026-04-07 23:24:04 +0300

    RCA-12: Правки после ребейза

--- app/src/main/java/com/yourcompany/recipecomposeapp/data/model/CategoryDto.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/data/model/CategoryDto.kt
@@ -1,9 +1,8 @@
 package com.yourcompany.recipecomposeapp.data.model
 
-data class RecipeDTO(
+data class CategoryDto(
     val id: Int,
     val title: String,
-    val ingredients: List<IngredientDTO>,
-    val method: List<String>,
+    val description: String,
     val imageUrl: String
 )
--- app/src/main/java/com/yourcompany/recipecomposeapp/data/model/IngredientDto.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/data/model/IngredientDto.kt
@@ -1,6 +1,6 @@
 package com.yourcompany.recipecomposeapp.data.model
 
-data class IngredientDTO(
+data class IngredientDto(
     val quantity: String,
     val unitOfMeasure: String,
     val description: String
